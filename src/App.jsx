c4bcb82711da28ef89fff9750f9d4639cd1fa890
Shakib <41319856+shakib97itis@users.noreply.github.com>
2025-10-19 13:28:24 +0600

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Styled from "styled-components";
+
+const H1 = Styled.h1`
+  font-size: 50px;
+  color: #4a90e2;
+`;
+
+const App = () => {
+  return <H1>The wild embrace</H1>;
+};
+
+export default App;
