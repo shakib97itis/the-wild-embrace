994d61d1ca6c0659abbffbcbe53fce1bccf0767a
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-29 13:58:54 +0600

    Refactor Cabins component: update Row to use vertical layout and integrate Show Cabin Form button with state management

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,27 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
         <p>Filter / Sort</p>
       </Row>
-      <Row>
+      <Row type="vertical">
         <CabinTable />
+        <Button
+          onClick={() => setShowForm((showForm) => !showForm)}
+          variation="primary"
+        >
+          Show Cabin Form
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://hrtwnsjpybjitscwhnxf.supabase.co'
+export const supabaseUrl = 'https://hrtwnsjpybjitscwhnxf.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhydHduc2pweWJqaXRzY3dobnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNDczMzUsImV4cCI6MjA3NjYyMzMzNX0.tEMek7Yd9LUfzqeaE32AzKQb3DOQ-2pXuwdCMks0V2I"
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase
