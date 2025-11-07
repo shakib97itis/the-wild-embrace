368e926ee9830a0ef274a65ac79066ba3ec633cd
Shakib <41319856+shakib97itis@users.noreply.github.com>
2025-11-08 00:47:26 +0600

    Refactor AddCabin component: simplify modal structure by removing unused CabinTable modal and organizing modal windows

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,25 +1,19 @@
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
 import Button from '../../ui/Button';
-import CabinTable from '../cabins/CabinTable';
 
 const AddCabin = () => {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="cabin-table">
-        <Button variation="secondary">Delete Cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 };
 
