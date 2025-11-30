0661fa5ec1754b2a504c15486ba62439db30e867
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-11-30 12:16:00 +0600

    feat: refactor Header component to include HeaderMenu and UserAvatar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logout from '../features/authentication/Logout';
+import UserAvatar from '../features/authentication/UserAvatar';
+import HeaderMenu from './HeaderMenu';
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2.4rem;
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 };
