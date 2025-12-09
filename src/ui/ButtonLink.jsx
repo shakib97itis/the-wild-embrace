d613044173f9e6b925b710d2d0b021dcd32f797c
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-12-09 14:14:23 +0600

    feat: add ButtonLink component for styled navigation buttons

---
+++ src/ui/ButtonLink.jsx
@@ -0,0 +1,72 @@
+import styled, {css} from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const sizes = {
+  small: css`
+    font-size: 1.2rem;
+    padding: 0.4rem 0.8rem;
+    text-transform: uppercase;
+    font-weight: 600;
+    text-align: center;
+  `,
+  medium: css`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+  `,
+  large: css`
+    font-size: 1.6rem;
+    padding: 1.2rem 2.4rem;
+    font-weight: 500;
+  `,
+};
+
+const variations = {
+  primary: css`
+    color: var(--color-brand-50);
+    background-color: var(--color-brand-600);
+
+    &:hover {
+      background-color: var(--color-brand-700);
+    }
+  `,
+  secondary: css`
+    color: var(--color-grey-600);
+    background: var(--color-grey-0);
+    border: 1px solid var(--color-grey-200);
+
+    &:hover {
+      background-color: var(--color-grey-50);
+    }
+  `,
+  danger: css`
+    color: var(--color-red-100);
+    background-color: var(--color-red-700);
+
+    &:hover {
+      background-color: var(--color-red-800);
+    }
+  `,
+};
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  border: none;
+
+  ${(props) => sizes[props.size || 'medium']}
+  ${(props) => variations[props.$variation || 'primary']}
+  ${(props) => props.disabled && 'cursor: not-allowed; opacity: 0.8;'}
+  width: 100%;
+`;
+
+const ButtonLink = ({variation, size, to, ...rest}) => {
+  return (
+    <Link to={to} style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <StyledButton $variation={variation} size={size} {...rest} />
+    </Link>
+  );
+};
+
+export default ButtonLink;
