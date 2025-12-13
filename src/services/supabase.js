9c8ca106c0e775ded19c82e91894d7e1f3bc64f4
Shakibul islam <41319856+shakib97itis@users.noreply.github.com>
2025-12-13 13:38:19 +0600

    feat: update supabase configuration

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,5 @@
 import {createClient} from '@supabase/supabase-js';
-export const supabaseUrl = 'https://hrtwnsjpybjitscwhnxf.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhydHduc2pweWJqaXRzY3dobnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNDczMzUsImV4cCI6MjA3NjYyMzMzNX0.tEMek7Yd9LUfzqeaE32AzKQb3DOQ-2pXuwdCMks0V2I';
+export const supabaseUrl = 'get it from env';
+const supabaseKey = 'get it from env';
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
